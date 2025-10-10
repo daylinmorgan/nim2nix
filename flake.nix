4d2345c6167dd547f0e6d355a063e69c7585ea01
Daylin Morgan <me@dayl.in>
2025-10-10 12:36:56 -0400

    move packages to checks

--- flake.nix
+++ flake.nix
@@ -2,31 +2,30 @@
   description = "nim2nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
     { nixpkgs, self, ... }:
     let
       inherit (nixpkgs.lib) genAttrs;
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems =
         fn:
-        genAttrs
-          [
-            "x86_64-linux"
-            "x86_64-darwin"
-            "aarch64-linux"
-            "aarch64-darwin"
-          ]
-          (
-            system:
-            fn (
-              import nixpkgs {
-                inherit system;
-                overlays = [ self.overlays.default ];
-              }
-            )
-          );
+        genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            }
+          )
+        );
     in
     {
       overlays = {
@@ -34,13 +33,9 @@
           buildNimblePackage = final.callPackage ./build-nimble-package.nix { };
         };
       };
-      packages = forAllSystems (pkgs: rec {
-        # buildNimblePackage = pkgs.callPackage ./pkgs/build-nimble-package.nix {};
-        # simple package to test functionality
-        fugitive = pkgs.callPackage ./pkgs/fugitive/package.nix { };
-        default = fugitive;
 
-        # failing because of the compiler
+      checks = forAllSystems (pkgs: {
+        fugitive = pkgs.callPackage ./pkgs/fugitive/package.nix { };
         nimlangserver = pkgs.callPackage ./pkgs/nimlangserver/package.nix { };
       });
 
