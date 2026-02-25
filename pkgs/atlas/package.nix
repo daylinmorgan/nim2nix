f0b3d91d0931be3ae880ec615d903a2f795f9d91
Daylin Morgan <me@dayl.in>
2026-02-25 13:45:13 -0500

    update atlas

--- pkgs/atlas/package.nix
+++ pkgs/atlas/package.nix
@@ -8,18 +8,15 @@
 buildNimPackage (
   final: prev: {
     pname = "atlas";
-    version = "unstable-2025-10-17";
+    version = "0.10.1";
     src = fetchFromGitHub {
       owner = "nim-lang";
       repo = "atlas";
-      rev = "3e33c732546760359b58a81df2269edd1d0c5f4d";
-      hash = "sha256-3hv5fEmoPR/Jt/x8Kpv41yVx3zbC0YK62I7a+20HFcM=";
+      rev = final.version;
+      hash = "sha256-WUnPvwsZ0IiDU3WhOQBUf2zT47jUFkZ0Kxn4oxWqdSU=";
     };
     lockFile = ./lock.json;
     buildInputs = [ openssl ];
-    # prePatch = ''
-    #   rm config.nims
-    #   '';
     postPatch = ''
       substituteInPlace src/atlas.nim \
         --replace-fail 'staticExec("git log -n 1 --format=%H")' '"${final.src.rev}"'
